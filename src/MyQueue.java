b9c3a1d55da1afd1f723d302b7ff56b8331cc258
BavyaChowdavarapu <111458969+BavyaChowdavarapu@users.noreply.github.com>
2023-10-31 13:21:38 -0500

    started working on myQueue

---
+++ src/MyQueue.java
@@ -0,0 +1,59 @@
+import java.util.NoSuchElementException;
+import java.util.*;
+
+public class MyQueue {
+    Queue<Square> nextStep = new Queue<>();
+
+ 
+    /**
+     * Add an item to the queue
+     * @param item the data item to add (of type T)
+     */
+    void enqueue(Square item){
+
+    }
+
+    /**
+     * Remove the front item from the queue
+     * @return the front item in the queue
+     * @throws NoSuchElementException if the queue is empty
+     */
+    Square dequeue() throws NoSuchElementException{
+
+    }
+
+    /**
+     * Return the front item in the queue without removing it
+     * @return the front item in the queue
+     * @throws NoSuchElementException if the queue is empty
+     */
+    Square front() throws NoSuchElementException{
+
+    }
+
+    /**
+     * Find how many items are in the queue
+     * @return the number of items in the queue
+     */
+    int size(){
+
+    }
+
+    /**
+     * Determine if the queue is empty
+     * @return true if the size is 0, false otherwise
+     */
+    boolean isEmpty(){
+
+    }
+
+    /**
+     * Clear out the data structure
+     */
+    void clear(){
+
+    }
+
+}
+
+
