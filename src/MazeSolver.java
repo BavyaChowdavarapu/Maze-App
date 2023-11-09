ba2e894278ff8fe76699ed651d766effcafc3809
sryerabati <sryerabati@stu.naperville203.org>
2023-11-09 13:17:59 -0600

    Update MazeSolver.java

--- src/MazeSolver.java
+++ src/MazeSolver.java
@@ -32,6 +32,7 @@ public abstract class MazeSolver {
         return path;
     }
 
+    /* 
     Square step(){
         if (!isEmpty()){
             Square explore = next();
@@ -59,7 +60,47 @@ public abstract class MazeSolver {
         }  
         return new Square(0,0,9);  
     }
+*/
 
+
+
+     Square step(){
+        if (isEmpty()){
+            // If the worklist is empty, there's nothing to explore, and the maze is unsolvable.
+            return null;  
+        }
+        
+        Square current = next(); // Get the next square to explore
+        
+        if (current.getType() == 3){ // Check if it's the end
+            // Trace back the path from end to start, marking it along the way
+            markPath(current);
+            return current;
+        }
+    
+        current.marked = true; // Mark the current square as visited
+    
+        ArrayList<Square> neighbors = myMaze.getNeighbors(current); // Get neighbors
+        for (Square neighbor : neighbors){
+            // If the neighbor has not been visited and is not a wall, add it to the worklist
+            if (!neighbor.marked && neighbor.getType() != 1){
+                add(neighbor);
+                neighbor.previous = current; // Set the previous to track the path
+            }
+        }
+        
+        return current;
+    }
+    
+    private void markPath(Square end){
+        Square current = end;
+        while (current.previous != null) {
+            //current.setType(Square.PATH); // Set the square as part of the path
+            current.marked = true;
+            current = current.previous; // Move to the previous square
+        }
+        //current.setType(Square.START); // Mark the starting square
+    }
 
     
     void solve(){
